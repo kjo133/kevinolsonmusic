2c8e5b54ee14988ec053999f1c21fc8ce1e33e0a
Kevin Olson <kevino93@vt.edu>
2021-01-14 15:18:40 -0500

    new web design for 2021

---
+++ js/youtubePlayer.js
@@ -0,0 +1,13 @@
+$('.videoImage').on('click', function(){
+    videoURL = $(this).attr('title');
+    console.log("vidoURL "+ videoURL);
+    $(this).html('<iframe width="100%" height="100%" src="' + videoURL + '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="youtubeVideo"></iframe>')
+});
+
+
+
+$('.acc').hover(function(){
+    console.log('hovered');
+},function(){
+    console.log('not hovered');
+})
